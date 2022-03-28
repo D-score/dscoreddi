f894279e1cb67dd2bf1a13236c3db77946ec77a7
Iris Eekhout <iriseekhout@users.noreply.github.com>
2022-03-28 17:11:51 +0200

    move part of the percentile calculation to global

--- inst/dscore_continue/server.R
+++ inst/dscore_continue/server.R
@@ -12,63 +12,24 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
+    #reference data, with additional column for start-items based on input$refperc en highlights voor geselecteerde start-items based on input$agemos en input$suggest.
+    # ahv voorgaand afgenomen items uit BDS item_candidates aanpassen en dynamisch maken, dan kunnen alleen items gesuggereerd worden die nog niet eerder zijn afgenomen; of herhaald moeten worden afgenomen.
+    # volgende stap is het aanpassen van de highlight variabele door de ingevoerde van wiechen kenmerken tijdens het consult.
     pref <- reactive({
-        #for start criterion
-        reference_percentile <- input$refperc
-        if(reference_percentile == 100) reference_percentile <- 99
-        if(reference_percentile == 0) reference_percentile <- 1
 
-
-        refdata <- calculate_age_equivalents(itembank = itembank, scalefactor = 2.099986, p = c(10, 50, 90, reference_percentile), reference = expanded_reference)
-        colnames(refdata)[c(5,9)]<- c("Dstart", "Pstart")
-
-        #load("data/itemtableVWO.rda")
-
-        prefdat <- refdata %>%
-            #pivot_longer(cols = c(everything(), -item), names_pattern = "(.)(.*)",
-            #             names_to = c(".value", "percentiel")) %>%
-            mutate(#percentiel = as.numeric(percentiel),
-                domain = substr(item, 4,5),
-                domein = plyr::revalue(domain, c("cm" = "Communicatie",
-                                                 "fm" = "Fijne motoriek",
-                                                 "gm" = "Grove motoriek"))) %>%
-            #rename(leeftijd = A) %>%
-            left_join(itemtableVWO %>% select(item, labelNL, ID.VWO2005, month), by = "item") %>%
-            mutate(nr = as.numeric(substr(item, 7, 9)),
-                   nr = ifelse(nr == 136, 35, nr),
-                   nr = ifelse(nr == 148, 40, nr),
-                   nr = ifelse(nr == 168, 68.1, nr),
-                   nr = ifelse(nr == 268, 68.2, nr),
-                   domein = ifelse(nr == 6, "Fijne motoriek", domein),
-
-                   labelNLn = paste(nr, labelNL, sep = ". "),
-                   labelNLn = ifelse(nr %in% 52:55, paste(nr, labelNL, sep = ".* "), labelNLn),
-                   labelNLn = ifelse(month < 10, paste(labelNLn, month, sep = " |  "), paste(labelNLn, month, sep = " |") ),
-                   sideA = ifelse(nr %in% c(1:12, 29:38, 52:67), 1, 0),
-                   sideB = ifelse(nr %in% c(11:28, 37:51, 66:75, 68.1, 68.2), 1, 0),
-                   A10 = ifelse(is.na(A10), 0, A10),
-                   A50 = ifelse(is.na(A50), 0, A50),
-                   A90 = ifelse(is.na(A90), 0, A90)
-
-            ) %>%
-            arrange(nr) %>%
-            mutate(closeness = abs(Pstart - input$agemos),
-                   closeness = ifelse(is.na(closeness), 999, closeness))
-
-        item_candidates <- prefdat$item
-
-        crit <- prefdat %>% filter(item %in% item_candidates) %>%
-            summarise(nth(sort(closeness), input$suggest))
-        crit
+         item_candidates <- prefdat %>% filter(sideA==1 | sideB ==1) %>% select(item)
+        itembank_candidates <- itembank %>% filter(item %in% item_candidates[[1]])
+        selected_items <- dinstrument::dform1(itembank = itembank_candidates, ageband = input$agemos, reference = expanded_reference, scalefactor = 2.099986, leniency = input$refperc, n = input$suggest)$item
 
             prefdat %>%
-            mutate(highlight = ifelse(item %in% item_candidates & closeness <= crit[[1]], "#1A80C4", "black"))
+            mutate(highlight = ifelse(item %in% selected_items, "#1A80C4", "black"))
 
 
 
 
     })
-### NU NOG STERRETJES IN DE PLOT WEERGEVEN
+
+    #
     output$VWOplot1 <- renderPlot({
 
         ## 0-18m
@@ -126,16 +87,8 @@ shinyServer(function(input, output) {
         m1 <- arrangeGrob(grobs = allplotslist1, nrow = 3, ncol = 1, heights = c(11,9,15))
         grid.arrange(m1)
 
-        allplotslist1 <- align_plots(fm1,cm1,gm1, align = "v")
-
-        m1 <- arrangeGrob(grobs = allplotslist1, nrow = 3, ncol = 1, heights = c(11,9,15))
-        grid.arrange(m1)
 
     })
-
-
-    ##### > 15m
-
     output$VWOplot2 <- renderPlot({
 
         fm_highlight <- pref() %>% filter(domein == "Fijne motoriek"& sideB == 1) %>%
@@ -149,7 +102,7 @@ shinyServer(function(input, output) {
         geom_hline(yintercept = input$agemos)+
         coord_flip()+ xlab("") +
         theme(legend.position = "none", axis.text.y = element_text(color = fm_highlight[[1]])) +
-        scale_y_continuous(name = "", breaks = seq(0,48,3), limits= c(8,48),position = "right")+
+        scale_y_continuous(name = "", breaks = seq(0,48,3), limits= c(8,50),position = "right")+
         ggtitle("Fijne motoriek/Adaptie/Persoonlijkheid en Sociaal gedrag")
 
     cm_highlight <- pref() %>% filter(domein == "Communicatie"& sideB == 1) %>%
@@ -163,7 +116,7 @@ shinyServer(function(input, output) {
         geom_hline(yintercept = input$agemos)+
         coord_flip()+ xlab("") +
         theme(legend.position = "none", axis.text.y = element_text(color = cm_highlight[[1]])) +
-        scale_y_continuous(name = "", breaks = seq(0,48,3), limits= c(8,48), position = "right")+
+        scale_y_continuous(name = "", breaks = seq(0,48,3), limits= c(8,50), position = "right")+
         ggtitle("Communicatie")
 
     gm_highlight <- pref() %>% filter(domein == "Grove motoriek"& sideB == 1) %>%
@@ -178,7 +131,7 @@ shinyServer(function(input, output) {
         coord_flip()+ xlab("") +
         theme(legend.position = "none", axis.text.y = element_text(color = gm_highlight[[1]])) +
         ylab("Leeftijd (maanden)") + xlab("") +
-        scale_y_continuous(name = "Leeftijd (maanden)", breaks = seq(0,48,3), limits= c(8,48),
+        scale_y_continuous(name = "Leeftijd (maanden)", breaks = seq(0,48,3), limits= c(8,50),
                            sec.axis = sec_axis(trans = ~(.*1), breaks = seq(0,48,3)))+
         ggtitle("Grove motoriek")
 
@@ -186,27 +139,23 @@ shinyServer(function(input, output) {
 
     allplotslist <- align_plots(fm2,cm2,gm2, align = "v")
 
-    m1 <- arrangeGrob(grobs = allplotslist, nrow = 3, ncol = 1, heights = c(10.5,9,8.5))
+    m1 <- arrangeGrob(grobs = allplotslist, nrow = 3, ncol = 1, heights = c(11.5,10,9))
 
 
     grid.arrange(m1)
 
 })
-
-
     output$Dscoreplot <- renderPlot({
 
         ggplot()+
             geom_line(data =references, aes(x = month, y = d, group = centile), color = "#1A80C4", size = 0.8, alpha = 0.5) +
-            geom_point(data = childdat, aes(x = month, y = d))+
-            geom_line(data = childdat, aes(x = month, y = d))+
+            geom_point(data = childdat, aes(x = month, y = d), size = 2)+
+            geom_line(data = childdat, aes(x = month, y = d), size = 1)+
 
             ylab("D-score") +
             scale_x_continuous("Leeftijd (in maanden)", limits = c(0,36),
-                               breaks = seq(0, 36, 6))
+                               breaks = seq(0, 36, 3))
 
     })
-
-
 
 })
