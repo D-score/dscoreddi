73e85acefcfbe959fe295ecb5e1d322ce33ca8f7
Iris Eekhout <iriseekhout@users.noreply.github.com>
2022-04-26 15:52:57 +0200

    move code to functions and functions to james

--- inst/dscore_continue/global.R
+++ inst/dscore_continue/global.R
@@ -32,51 +32,12 @@ itembank <- james_get(host, path = "itembank")$parsed %>%
 
 
 # get expanded reference
-resp <- james_get(host, path = "expanded_reference", query = "population=dutch")
+resp <- james_get(host, path = "expand_reference", query = "population=dutch")
 expanded_reference <- resp$parsed
 
 
 expanded_reference <- dscoreddi::expand_reference(population = "dutch")
 
-
-# execute a post request
-refdata <- james_post(host = host,
-                      path = "get_age_equivalents",
-                      itembank = itembank,
-                      scalefactor = 2.099986,
-                      reference = expanded_reference)$parsed
-
-itemtableVWO <- james_get(host = host, path = "itemtableVWO")$parsed
-
-prefdat <- refdata %>%
-  mutate(
-    domain = substr(item, 4,5),
-    domein = dplyr::recode(domain,
-                           "cm" = "Communicatie",
-                           "fm" = "Fijne motoriek",
-                           "gm" = "Grove motoriek")) %>%
-  #rename(leeftijd = A) %>%
-  left_join(itemtableVWO %>% select(item, labelNL, ID.VWO2005, month), by = "item") %>%
-  mutate(nr = as.numeric(substr(item, 7, 9)),
-         nr = ifelse(nr == 136, 35, nr),
-         nr = ifelse(nr == 148, 40, nr),
-         nr = ifelse(nr == 068, 68.1, nr),
-         nr = ifelse(nr == 168, 68.2, nr),
-         nr = ifelse(nr == 268, 68.3, nr),
-         domein = ifelse(nr == 6, "Fijne motoriek", domein),
-
-         labelNLnr = paste(nr, labelNL, sep = ". "),
-         #labelNLn = ifelse(nr %in% 52:55, paste(nr, labelNL, sep = ".* "), labelNLn),
-         labelNLn = ifelse(month < 10, paste(labelNLnr, month, sep = " |  "),
-                           paste(labelNLnr, month, sep = " |") ),
-         labelNLn = ifelse(nr %in% 52:55, paste(labelNLnr, "**", sep = " | "), labelNLn),
-         sideA = ifelse(nr %in% c(1:12, 29:38, 52:67), 1, 0),
-         sideB = ifelse(nr %in% c(11:28, 37:51, 66:75, 68.1, 68.2), 1, 0),
-         A10 = ifelse(is.na(A10), 0, A10),
-         A50 = ifelse(is.na(A50), 0, A50),
-         A90 = ifelse(is.na(A90), 0, A90)
-  ) %>%
-  arrange(nr)
 
 
 # child data for demo
--- inst/dscore_continue/server.R
+++ inst/dscore_continue/server.R
@@ -17,146 +17,33 @@ shinyServer(function(input, output) {
     # volgende stap is het aanpassen van de highlight variabele door de ingevoerde van wiechen kenmerken tijdens het consult.
     pref <- reactive({
 
-         item_candidates <- prefdat %>% filter(sideA==1 | sideB ==1) %>% select(item)
-        itembank_candidates <- itembank %>% filter(item %in% item_candidates[[1]])
-        selected_items <- dinstrument::dform1(itembank = itembank_candidates, ageband = input$agemos, reference = expanded_reference, scalefactor = 2.099986, leniency = input$refperc, n = input$suggest)$item
+                   james_post(host = host,
+                             path = "get_percentile_plot_data",
+                             itembank = itembank,
+                             reference = expanded_reference,
+                             age = input$agemos,
+                             refperc = input$refperc,
+                             suggest = input$suggest)$parsed
 
-            prefdat %>%
-            mutate(highlight = ifelse(item %in% selected_items, "#e6550d", "black"),
-                   bold = ifelse(item %in% selected_items, "bold", "plain"))
-
-            #1A80C4 - blue color
-            #31a354 - green color
-            #e6550d - red color
     })
 
-    #
     output$VWOplot1 <- renderPlot({
-
-        ## 0-18m
-        fm_highlight <- pref() %>% filter(domein == "Fijne motoriek"& sideA == 1) %>%
-            arrange(-nr) %>% select(highlight) #added
-        fm_bold <- pref() %>% filter(domein == "Fijne motoriek"& sideA == 1) %>%
-          arrange(-nr) %>% select(bold)
-        fm1 <-
-            ggplot(pref() %>% filter(domein == "Fijne motoriek"& sideA == 1),  #selecteer milestons met A10 < 15 maanden
-                   aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight)) + #changed
-            geom_point()+
-            geom_errorbar(aes(ymin = A10, ymax = A90))+
-            scale_color_manual(values = c("black" = "black", "#e6550d" = "#e6550d"))+ #added
-            geom_hline(yintercept = input$agemos)+
-            coord_flip()+ xlab("") +
-            theme(legend.position = "none", axis.text.y = element_text(face = fm_bold[[1]], color = fm_highlight[[1]])) +
-            scale_y_continuous(name = "", breaks = c(0,1,2,3,4,5,6,7,8,9,10, seq(12,48,3)), limits= c(0,18),position = "right")+
-            ggtitle("Fijne motoriek/Adaptie/Persoonlijkheid en Sociaal gedrag")
-
-
-        cm_highlight <- pref() %>% filter(domein == "Communicatie"& sideA == 1) %>%
-            arrange(-nr) %>% select(highlight) #added
-        cm_bold <- pref() %>% filter(domein == "Communicatie"& sideA == 1) %>%
-          arrange(-nr) %>% select(bold)
-        cm1 <-
-            ggplot(pref() %>% filter(domein == "Communicatie"& sideA == 1),#selecteer milestons met A10 < 15 maanden
-                   aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight)) +
-            geom_point()+
-            geom_errorbar(aes(ymin = A10, ymax = A90))+
-            scale_color_manual(values = c("black" = "black", "#e6550d" = "#e6550d"))+ #added
-            geom_hline(yintercept = input$agemos)+
-            coord_flip() + xlab("") +
-            theme(legend.position = "none", axis.text.y = element_text(face = cm_bold[[1]], color = cm_highlight[[1]])) +
-            scale_y_continuous(name = "", breaks = c(0,1,2,3,4,5,6,7,8,9,10, seq(12,48,3)), limits= c(0,18), position = "right")+
-            ggtitle("Communicatie")
-
-
-        gm_highlight <- pref() %>% filter(domein == "Grove motoriek"& sideA == 1) %>%
-            arrange(-nr) %>% select(highlight) #added
-        gm_bold <- pref() %>% filter(domein == "Grove motoriek"& sideA == 1) %>%
-          arrange(-nr) %>% select(bold)
-
-        gm1 <-
-            ggplot(pref() %>% filter(domein == "Grove motoriek" & sideA == 1), #selecteer milestons met A10 < 15 maanden
-                   aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight)) +
-            geom_point()+
-            geom_errorbar(aes(ymin = A10, ymax = A90))+
-            scale_color_manual(values = c("black" = "black", "#e6550d" = "#e6550d"))+ #added
-            geom_hline(yintercept = input$agemos)+
-            coord_flip()+
-            ylab("Leeftijd (maanden)") + xlab("") +
-            theme(legend.position = "none", axis.text.y = element_text(face = gm_bold[[1]], color = gm_highlight[[1]])) +
-            scale_y_continuous(name = "Leeftijd (maanden)", breaks = c(0,1,2,3,4,5,6,7,8,9,10, seq(12,48,3)), limits= c(0,18),
-                               sec.axis = sec_axis(trans = ~(.*1), breaks = c(0,1,2,3,4,5,6,7,8,9,10, seq(12,48,3))))+
-            ggtitle("Grove motoriek")
-
-        allplotslist1 <- align_plots(fm1,cm1,gm1, align = "v")
-
-        m1 <- arrangeGrob(grobs = allplotslist1, nrow = 3, ncol = 1, heights = c(11,9,15))
-        grid.arrange(m1)
-
-
+      vwo_plot(data = pref(), side = "sideA", age = input$age)
+      james_post(host = host,
+                 path = "get_vwo_plot",
+                 data = pref(),
+                 side = "sideA",
+                 age = input$agemos)$parsed
     })
+
+
     output$VWOplot2 <- renderPlot({
-
-        fm_highlight <- pref() %>% filter(domein == "Fijne motoriek"& sideB == 1) %>%
-            arrange(-nr) %>% select(highlight) #added
-        fm_bold <- pref() %>% filter(domein == "Fijne motoriek"& sideB == 1) %>%
-          arrange(-nr) %>% select(bold)
-
-    fm2 <-
-        ggplot(pref() %>% filter(domein == "Fijne motoriek"& sideB == 1), #selecteer milestons met A10 < 15 maanden
-               aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight)) +
-        geom_point()+
-        geom_errorbar(aes(ymin = A10, ymax = A90))+
-        scale_color_manual(values = c("black" = "black", "#e6550d" = "#e6550d"))+ #added
-        geom_hline(yintercept = input$agemos)+
-        coord_flip()+ xlab("") +
-        theme(legend.position = "none", axis.text.y = element_text(face = fm_bold[[1]], color = fm_highlight[[1]])) +
-        scale_y_continuous(name = "", breaks = seq(0,48,3), limits= c(8,50),position = "right")+
-        ggtitle("Fijne motoriek/Adaptie/Persoonlijkheid en Sociaal gedrag")
-
-    cm_highlight <- pref() %>% filter(domein == "Communicatie"& sideB == 1) %>%
-        arrange(-nr) %>% select(highlight) #added
-    cm_bold <- pref() %>% filter(domein == "Communicatie"& sideB == 1) %>%
-      arrange(-nr) %>% select(bold)
-    cm2 <-
-        ggplot(pref() %>% filter(domein == "Communicatie"& sideB == 1),#selecteer milestons met A10 < 15 maanden
-               aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight)) +
-        geom_point()+
-        geom_errorbar(aes(ymin = A10, ymax = A90))+
-        scale_color_manual(values = c("black" = "black", "#e6550d" = "#e6550d"))+ #added
-        geom_hline(yintercept = input$agemos)+
-        coord_flip()+ xlab("") +
-        theme(legend.position = "none", axis.text.y = element_text(face = cm_bold[[1]], color = cm_highlight[[1]])) +
-        scale_y_continuous(name = "", breaks = seq(0,48,3), limits= c(8,50), position = "right")+
-        ggtitle("Communicatie")
-
-    gm_highlight <- pref() %>% filter(domein == "Grove motoriek"& sideB == 1) %>%
-        arrange(-nr) %>% select(highlight) #added
-    gm_bold <- pref() %>% filter(domein == "Grove motoriek"& sideB == 1) %>%
-      arrange(-nr) %>% select(bold)
-    gm2 <-
-        ggplot(pref() %>% filter(domein == "Grove motoriek" & sideB == 1), #selecteer milestons met A10 < 15 maanden
-               aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight)) +
-        geom_point()+
-        geom_errorbar(aes(ymin = A10, ymax = A90))+
-        scale_color_manual(values = c("black" = "black", "#e6550d" = "#e6550d"))+ #added
-        geom_hline(yintercept = input$agemos)+
-        coord_flip()+ xlab("") +
-        theme(legend.position = "none", axis.text.y = element_text(face = gm_bold[[1]], color = gm_highlight[[1]])) +
-        ylab("Leeftijd (maanden)") + xlab("") +
-        scale_y_continuous(name = "Leeftijd (maanden)", breaks = seq(0,48,3), limits= c(8,50),
-                           sec.axis = sec_axis(trans = ~(.*1), breaks = seq(0,48,3)))+
-        ggtitle("Grove motoriek")
+      vwo_plot(data = pref(), side = "sideB", age = input$age)
+      })
 
 
 
-    allplotslist <- align_plots(fm2,cm2,gm2, align = "v")
 
-    m1 <- arrangeGrob(grobs = allplotslist, nrow = 3, ncol = 1, heights = c(11.5,10,9))
-
-
-    grid.arrange(m1)
-
-})
     output$Dscoreplot <- renderPlot({
 
         ggplot()+
