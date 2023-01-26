4e33ee2b1a0adf349eb92668c9283fc26e35bb5a
Iris Eekhout <iriseekhout@users.noreply.github.com>
2023-01-26 10:38:08 +0100

    update text by suggestions Sylvia

--- inst/dscore_continue/ui.R
+++ inst/dscore_continue/ui.R
@@ -14,7 +14,7 @@ shinyUI(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(width = 2,
-            p("Deze applicatie selecteert op basis van leeftijd en percentiel (regulier is ongeveer 90 en gemiddeld is ongeveer 50) het wenste aantal aanbevolen van Wiechen kenmerken om af te nemen in blauw."),
+            p("Dit is een tool om te helpen met het selecteren van Van Wiechen items per kind. Dit kun je doen door hieronder Leeftijd, Percentiel (oorspronkelijk gaat Van Wiechen uit van P90 en dat 90% van de kinderen het moet kunnen om achterstand op te sturen, je kunt uitgaan van P50 als je wilt weten wat een gemiddeld kind op deze leeftijd laat zien) en het aantal Van Wiechen kenmerken dat je wenst af te nemen."),
             br(),
 
             numericInput("agemos", "Leeftijd kind in maanden", min = 0, max = 50, value = 6),
@@ -38,28 +38,39 @@ shinyUI(
             tabsetPanel(
                 tabPanel("VWO 0-15m",
                          plotOutput("VWOplot1", height = 800),
-                         p("** = Kenmerk herhalen; (M) = mededeling; -rl = rechts en links samen"),
-                         p("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind. Kenmerken die passen bij de instellingen zijn gekleurd.")
+                         tags$div(
+                           tags$span("(M) = mededeling; -rl = rechts en links samen."), tags$br(),
+                           tags$span("** = Kenmerk herhalen gedurende eerste 52 weken.", style = "color:#3399ff;"), tags$span("Kenmerken die passen bij de instellingen zijn gekleurd.", style= "color:#e6550d"),tags$br(), tags$span("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind.")
+                         )
                          ),
                 tabPanel("VWO 12-48m",
                          plotOutput("VWOplot2", height = 800),
-                         p("(M) = mededeling; -rl = rechts en links samen"),
-                         p("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind. Kenmerken die passen bij de instellingen zijn gekleurd.")
+                         tags$div(
+                           tags$span("(M) = mededeling; -rl = rechts en links samen."), tags$br(),
+                           tags$span("Kenmerken die passen bij de instellingen zijn gekleurd.", style= "color:#e6550d"),tags$br(), tags$span("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind.")
+                         )
                          ),
                 tabPanel("VWO Fijne motoriek",
                          plotOutput("VWOfinemotor", height = 800),
-                         p("** = Kenmerk herhalen; (M) = mededeling; -rl = rechts en links samen"),
-                         p("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind. Kenmerken die passen bij de instellingen zijn gekleurd.")
+                         tags$div(
+                           tags$span("(M) = mededeling; -rl = rechts en links samen."), tags$br(),
+                           tags$span("Kenmerken die passen bij de instellingen zijn gekleurd.", style= "color:#e6550d"),tags$br(), tags$span("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind.")
+                         )
                 ),
                 tabPanel("VWO Communicatie",
                          plotOutput("VWOcommunication", height = 800),
-                         p("(M) = mededeling; -rl = rechts en links samen"),
-                         p("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind. Kenmerken die passen bij de instellingen zijn gekleurd.")
+                         tags$div(
+                           tags$span("(M) = mededeling; -rl = rechts en links samen."), tags$br(),
+                           tags$span("Kenmerken die passen bij de instellingen zijn gekleurd.", style= "color:#e6550d"),tags$br(), tags$span("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind.")
+                         )
                 ),
                 tabPanel("VWO Grove motoriek",
                          plotOutput("VWOgrovemotor", height = 800),
-                         p("** = Kenmerk herhalen; (M) = mededeling; -rl = rechts en links samen"),
-                         p("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind. Kenmerken die passen bij de instellingen zijn gekleurd.")
+                         tags$div(
+                          tags$span("(M) = mededeling; -rl = rechts en links samen."), tags$br(),
+                          tags$span("** = Kenmerk herhalen gedurende eerste 52 weken.", style = "color:#3399ff;"), tags$span("Kenmerken die passen bij de instellingen zijn gekleurd.", style= "color:#e6550d"),tags$br(), tags$span("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind.")
+                         )
+
                 ),
                 tabPanel("D-score curve", plotOutput("Dscoreplot", height = 800))
             )
