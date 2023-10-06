148a550f8d79b8e8ea9ef3451f13634c200ae681
Iris Eekhout <iriseekhout@users.noreply.github.com>
2023-10-06 10:00:03 +0200

    update test shiny with plotly plot for speed test to compare to ggplot version.
    turn one of each fully off by commenting to test performance

--- inst/shinytest/server.R
+++ inst/shinytest/server.R
@@ -25,19 +25,63 @@ function(input, output, session) {
          pivot_longer(names_to = "centile", values_to = "d", cols = -month)
 
 
-    output$distPlot <- renderPlot({
+
+    output$ggplottest <- renderPlot({
 
 
-           ggplot()+
-               geom_line(data =references, aes(x = month, y = d, group = centile), color = "#e6550d", size = 0.8, alpha = 0.5) +
-               geom_point(data = childdat, aes(x = month, y = d), size = 2)+
-               geom_line(data = childdat, aes(x = month, y = d), size = 1)+
+                  ggplot()+
+                      geom_line(data =references, aes(x = month, y = d, group = centile), color = "#e6550d", size = 0.8, alpha = 0.5) +
+                      geom_point(data = childdat, aes(x = month, y = d), size = 2)+
+                      geom_line(data = childdat, aes(x = month, y = d), size = 1)+
 
-               ylab("D-score") +
-               scale_x_continuous("Leeftijd (in maanden)", limits = c(0,36),
-                                  breaks = seq(0, 36, 3))
+                      ylab("D-score") +
+                      scale_x_continuous("Leeftijd (in maanden)", limits = c(0,36),
+                                         breaks = seq(0, 36, 3))
 
 
-          })
+
+
+
+    })
+
+    output$plotlytest <- renderPlotly({
+
+      plot_ly() %>%
+
+        # Voeg lijnen toe van de referentiedata
+        add_lines(data = references %>% filter(centile == "SDM2"), x = ~month, y = ~d, color = I("#e6550d"),
+                  alpha = 0.5, line = list(shape = "linear"),
+                  text = ~centile, hoverinfo = "text", showlegend = FALSE) %>%
+        add_lines(data = references %>% filter(centile == "SDM1"), x = ~month, y = ~d, color = I("#e6550d"),
+                  alpha = 0.5, line = list(shape = "linear"),
+                  text = ~centile, hoverinfo = "text", showlegend = FALSE) %>%
+        add_lines(data = references %>% filter(centile == "SD0"), x = ~month, y = ~d, color = I("#e6550d"),
+                  alpha = 0.5, line = list(shape = "linear"),
+                  text = ~centile, hoverinfo = "text", showlegend = FALSE) %>%
+        add_lines(data = references %>% filter(centile == "SDP1"), x = ~month, y = ~d, color = I("#e6550d"),
+                  alpha = 0.5, line = list(shape = "linear"),
+                  text = ~centile, hoverinfo = "text", showlegend = FALSE) %>%
+        add_lines(data = references %>% filter(centile == "SDP2"), x = ~month, y = ~d, color = I("#e6550d"),
+                  alpha = 0.5, line = list(shape = "linear"),
+                  text = ~centile, hoverinfo = "text", showlegend = FALSE) %>%
+
+        # Voeg punten toe van de kinddata
+        add_markers(data = childdat, x = ~month, y = ~d, mode = "markers") %>%
+
+        # Voeg lijnen toe van de kinddata
+        add_lines(data = childdat, x = ~month, y = ~d,
+                  line = list(shape = "linear"), showlegend = FALSE) %>%
+
+        # Voeg labels toe aan de y-as
+        layout(yaxis = list(title = "D-score")) %>%
+
+        # Pas de x-as aan
+        layout(xaxis = list(title = "Leeftijd (in maanden)", range = c(0, 36),
+                            tickvals = seq(0, 36, 3)))
+
+
+
+    })
+
 
 }
