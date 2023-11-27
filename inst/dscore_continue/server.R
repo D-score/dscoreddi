73b146ec88c948f7edb18e4701abefafe5b41337
Iris Eekhout <iriseekhout@gmail.com>
2023-11-27 11:57:24 +0100

    plotly versions for the domain specific plots

--- inst/dscore_continue/server.R
+++ inst/dscore_continue/server.R
@@ -192,12 +192,13 @@ shinyServer(function(input, output) {
 
     grid.arrange(m1)
 
+
 })
 
 
 
 #####----- Plot milestones for Fine motor
-    output$VWOfinemotor <- renderPlot({
+    output$VWOfinemotor <- renderPlotly({
 
       fm_highlight <- pref() %>% filter(domein == "Fijne motoriek") %>%
         arrange(-nr) %>% pull(highlight) #added
@@ -206,7 +207,7 @@ shinyServer(function(input, output) {
 
       fm_dom <-
         ggplot(pref() %>% filter(domein == "Fijne motoriek"), #selecteer milestons met A10 < 15 maanden
-               aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight)) +
+               aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight, label = labelNL)) +
         geom_point()+
         geom_errorbar(aes(ymin = A10, ymax = A90))+
         scale_color_manual(values = c("black" = "black", "#e6550d" = "#e6550d", "grey60" = "grey60"))+ #added
@@ -217,10 +218,11 @@ shinyServer(function(input, output) {
         xlab("")+
         ggtitle("Fijne motoriek/Adaptie/Persoonlijkheid en Sociaal gedrag")
 
+      fm_dom <- ggplotly(fm_dom, tooltip = c("label"))
       fm_dom
     })
 
-    output$VWOcommunication <- renderPlot({
+    output$VWOcommunication <- renderPlotly({
 
       cm_highlight <- pref() %>% filter(domein == "Communicatie") %>%
         arrange(-nr) %>% pull(highlight) #added
@@ -229,7 +231,7 @@ shinyServer(function(input, output) {
 
       cm_dom <-
         ggplot(pref() %>% filter(domein == "Communicatie"),#selecteer milestons met A10 < 15 maanden
-               aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight)) +
+               aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight, label = labelNL)) +
         geom_point()+
         geom_errorbar(aes(ymin = A10, ymax = A90))+
         scale_color_manual(values = c("black" = "black", "#e6550d" = "#e6550d", "grey60" = "grey60"))+ #added
@@ -240,28 +242,30 @@ shinyServer(function(input, output) {
         scale_y_continuous(name = "", breaks = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, seq(15,51,3)), minor_breaks = c(0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5,16,17,19,20,22,23,25,26,28,29,31,32,34,35,37,38,40,41,43,44,46,47,49,50), limits= c(0,50),position = "right", sec.axis= dup_axis(name = "Leeftijd (maanden)"), expand = c(0,0.1))+
         ggtitle("Communicatie")
 
+      cm_dom <- ggplotly(cm_dom, tooltip = "label")
       cm_dom
     })
 
-  output$VWOgrovemotor <- renderPlot({
+  output$VWOgrovemotor <- renderPlotly({
       gm_highlight <- pref() %>% filter(domein == "Grove motoriek") %>%
         arrange(-nr) %>% pull(highlight) #added
       gm_bold <- pref() %>% filter(domein == "Grove motoriek") %>%
         arrange(-nr) %>% pull(bold)
+
      gm_dom <-
         ggplot(pref() %>% filter(domein == "Grove motoriek"), #selecteer milestons met A10 < 15 maanden
-               aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight)) +
-        geom_point()+
-        geom_errorbar(aes(ymin = A10, ymax = A90))+
-        geom_linerange(aes(ymin = cont1, ymax = cont2), lty = 2)+
-        scale_color_manual(values = c("black" = "black", "#e6550d" = "#e6550d", "#3399ff"= "#3399ff", "grey60" = "grey60"))+ #added
-        geom_hline(yintercept = as.numeric(input$agemos))+
+               aes(x = reorder(labelNLn, -nr),  y= A50, group = highlight, color = highlight, label = labelNL)) +
+       geom_point()+
+       geom_errorbar(aes(ymin = A10, ymax = A90))+
+       scale_color_manual(values = c("black" = "black", "#e6550d" = "#e6550d", "grey60" = "grey60"))+ #added
+       geom_hline(yintercept = as.numeric(input$agemos))+
         coord_flip()+
         xlab("") +
         theme(legend.position = "none", axis.text.y = element_text(face = gm_bold, color = gm_highlight)) +
        scale_y_continuous(name = "", breaks = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, seq(15,51,3)), minor_breaks = c(0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5,16,17,19,20,22,23,25,26,28,29,31,32,34,35,37,38,40,41,43,44,46,47,49,50), limits= c(0,50),position = "right", sec.axis= dup_axis(name = "Leeftijd (maanden)"), expand = c(0,0.1))+
         ggtitle("Grove motoriek")
 
+      gm_dom <- ggplotly(gm_dom, tooltip = "label")
       gm_dom
 
 
--- inst/dscore_continue/ui.R
+++ inst/dscore_continue/ui.R
@@ -58,21 +58,21 @@ shinyUI(
                          )
                          ),
                 tabPanel("VWO Fijne motoriek",
-                         plotOutput("VWOfinemotor", height = 800),
+                         plotlyOutput("VWOfinemotor", height = 800),
                          tags$div(
                            tags$span("(M) = mededeling; -rl = rechts en links samen."), tags$br(),
                            tags$span("Kenmerken die passen bij de instellingen zijn gekleurd.", style= "color:#e6550d"),tags$br(), tags$span("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind.")
                          )
                 ),
                 tabPanel("VWO Communicatie",
-                         plotOutput("VWOcommunication", height = 800),
+                         plotlyOutput("VWOcommunication", height = 800),
                          tags$div(
                            tags$span("(M) = mededeling; -rl = rechts en links samen."), tags$br(),
                            tags$span("Kenmerken die passen bij de instellingen zijn gekleurd.", style= "color:#e6550d"),tags$br(), tags$span("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind.")
                          )
                 ),
                 tabPanel("VWO Grove motoriek",
-                         plotOutput("VWOgrovemotor", height = 800),
+                         plotlyOutput("VWOgrovemotor", height = 800),
                          tags$div(
                           tags$span("(M) = mededeling; -rl = rechts en links samen."), tags$br(),
                           tags$span("** = Kenmerk herhalen gedurende eerste 52 weken.", style = "color:#3399ff;"), tags$span("Kenmerken die passen bij de instellingen zijn gekleurd.", style= "color:#e6550d"),tags$br(), tags$span("Voor elk kenmerk geeft de balk de P10 (links), P50 (middenstip), P90 (rechts). De verticale hulplijn staat bij de leeftijd van het kind.")
