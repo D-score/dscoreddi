ab8554879f977b46bb707218272bdd9831e23017
Iris Eekhout <iriseekhout@gmail.com>
2024-09-20 13:48:15 +0200

    voorbeelden om van wiechen data uit DD registraties te cleanen om vervolgens D-score te berekenen. Voorbeeld gemaakt voor Laurens van Buren (JGZ Utrecht) en Erasmus

---
+++ scripts/vwd_cleaning_update.R
@@ -0,0 +1,118 @@
+#VWD cleaning script (voorbeeld)
+
+# Iris Eekhout
+# iris.eekhout@tno.nl
+# 12-7-2024
+
+
+# D-score berekenen met cleaningstappen voor JGZ registratie data
+#
+# Disclaimer: let op dat je bij doorscoren (doorplussen en terugnemen) wel bias introduceert, omdat je slechts voor een deel van de respondenten extra item responsen imputeert. Je kunt namelijk alleen terugscoren als een kenmerk later niet is behaald, en alleen doorscoren als een kenmerk eerder wel is behaald. En je doet hierbij de aanname dat het onmogelijk is om een niet behaald kenmerk eerder wel behaald te hebben, of een reeds behaald kenmerk later toch niet meer te halen. Hoe dan ook vul je fictieve observaties in en doe je alsof dit werkelijke observaties zijn (de geimputeerde scores worden namelijk op dezelfde manier behandeld/gewogen/gebruikt als geobserveerde scores.
+# Als je deze aannames niet kunt/ niet wilt doen, gebruik dan de D-scores op basis van de geobserveerde data uit regel 25.
+#
+
+library(dplyr)
+library(dscore)
+library(ggplot2)
+
+vwd <- childdevdata::gcdg_nld_smocc %>%
+  mutate(agemos = agedays/365.25 * 12,
+         age = agemos / 12)
+
+set.seed(62548)
+subselection <- sample(unique(vwd$subjid), 100)
+
+vwd <- vwd %>% filter(subjid %in% subselection)
+
+#D-score obv geobserveerde/geregistreerde data
+dgsed <- vwd %>%
+  dscore::dscore(data = .,
+                 items = colnames(vwd)[grepl("^ddi",colnames(vwd))],
+                 itembank = dscore::builtin_itembank[dscore::builtin_itembank$key == "gsed2212",],
+                 key = "gsed2212",
+                 population = "dutch")
+
+#Selecteer alleen D-scores met minimaal 3 items
+dgsedc <- dgsed %>% filter(n >= 3)
+
+
+ggplot(dgsedc, aes(a, d))+
+  geom_point() +
+  ggtitle("D-score voor leeftijd (ruwe data)")
+
+
+ggplot(dgsedc, aes(a, n))+
+  geom_point() +
+  ggtitle("Aantal kenmerken in data voor leeftijd (ruwe data)")
+
+
+
+ggplot(dgsedc, aes(a, p))+
+  geom_point() +
+  ggtitle("Kans op behalen van afgenomen kenmerken voor leeftijd (ruwe data)")
+
+
+
+
+## Leeftijdslimiet per leeftijd - Voor elk item is een maand voorgeschreven in het Van Wiechen schema waarop deze afgenomen zou moeten worden. Voor het doorscoren gebruiken we een leeftijdsrange van het de leeftijd op het contactmoment ervoor tot de leeftijd van het contact moment erna.
+## Deze restrictie geldt dus ALLEEN voor het doorscoren/terugminnen en niet voor de geobserveerde/geregisteerde data.
+cm_max <- data.frame(
+  month =   c(1, 2, 3, 6,  9, 12, 15, 18, 24, 30, 36, 42, 48, 55),
+  max_lft = c(2, 3, 6, 9, 12, 15, 18, 24, 30, 36, 42, 48, 55, 78),
+  min_lft = c(0, 1, 2, 3,  6,  9,  12, 15, 18, 24, 30, 36, 42, 48 )
+)
+
+itemtableVWO <- dscoreddi::itemtableVWO %>% select(item, labelNL, occ, month) %>%
+  left_join(cm_max, by = "month") %>% select(item, labelNL, occ, month, min_lft, max_lft)
+
+itemtableVWO
+
+library(tidyr)
+
+vwd_add_limit <-
+vwd %>%
+ pivot_longer(cols = starts_with("ddi"), names_to ="item", values_to = "score") %>%
+  left_join(itemtableVWO %>% select(item, min_lft, max_lft), by = "item") %>%
+  group_by(subjid, item) %>%
+  mutate(max_age_zero = ifelse(any(score == 0), max(agemos[which(score == 0)]), -Inf),
+         min_age_one = ifelse(any(score == 1), min(agemos[which(score == 1)]), Inf),
+
+         score = ifelse(is.na(score) & agemos < max_age_zero & agemos > min_lft, 0, score),
+         score = ifelse(is.na(score) & agemos > min_age_one & agemos < max_lft, 1, score)
+         ) %>%
+  select(-max_age_zero, -min_age_one, -min_lft, -max_lft) %>%
+  ungroup() %>%
+  pivot_wider(names_from = "item", values_from = "score")
+
+
+#dscore bepalen obv doorgescoorde items
+dgsed_add_limit <- vwd_add_limit %>%
+  dscore::dscore(data = .,
+                 items = colnames(vwd_add_limit)[grepl("^ddi",colnames(vwd_add_limit))],
+                 itembank = dscore::builtin_itembank[dscore::builtin_itembank$key == "gsed2212",],
+                 key = "gsed2212",
+                 population = "dutch")
+
+dgsed_add_limitc <- dgsed_add_limit %>% filter(n >= 3)
+
+
+ggplot(dgsed_add_limitc, aes(a, d))+
+  geom_point() +
+  ggtitle("D-score voor leeftijd (doorscoren met leeftijdslimiet)")
+
+
+ggplot(dgsed_add_limitc, aes(a, n))+
+  geom_point() +
+  ggtitle("Aantal kenmerken in data voor leeftijd (doorscoren met leeftijdslimiet)")
+
+
+
+ggplot(dgsed_add_limitc, aes(a, p))+
+  geom_point() +
+  ggtitle("Kans op behalen van afgenomen kenmerken voor leeftijd (doorscoren met leeftijdslimiet)")
+
+
+
+#vergelijking (zonder n >= 3 filter, zodat vergelijking kan)
+
+plot(dgsed$daz, dgsed_add_limit$daz)
